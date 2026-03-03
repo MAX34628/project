da4421d52136365ebf2eb04d26e6027af1387061
Максим <tchernov.ct2009@gmail.com>
2026-03-04 00:24:43 +0300

    Dev1: изменил приветствие

--- Hello.cs
+++ Hello.cs
@@ -2,5 +2,6 @@ public static class Hello
 {
     public static void SayHello()
     {
-        System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Hello from both!.");
     }
+}
